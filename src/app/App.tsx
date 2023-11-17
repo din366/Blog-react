3c312209c08a1f71a794b5ecc6fbd62f9e9ca7f1
Alex Din <din366@gmail.com>
2023-11-17 17:51:38 +0300

    moving and encapsulating routing into shared/config -> routeConfig.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,8 +3,7 @@ import './styles/index.scss';
 import {Link, Route, Routes} from "react-router-dom";
 import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
-import {AboutPage} from "pages/AboutPage";
-import {MainPage} from "pages/MainPage";
+import {AppRouter} from "app/providers/router";
 
 
 const App = () => {
@@ -16,12 +15,7 @@ const App = () => {
         <button onClick={toggleTheme}>Toggle Theme</button>
         <Link to={'/'}>Главная</Link>
         <Link to={'/about'}>О нас</Link>
-          <Suspense fallback={<div>loading...</div>}>
-            <Routes>
-              <Route path={'/about'} element={<AboutPage />}/>
-              <Route path={'/'} element={<MainPage />}/>
-            </Routes>
-          </Suspense>
+        <AppRouter />
       </div>
     </div>
 
