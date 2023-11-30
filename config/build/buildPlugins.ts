59a8d4b4f6a76eca911cfdd485b7b4f2cffbffc7
Alex Din <din366@gmail.com>
2023-11-30 13:56:34 +0300

    fix buildPlugins.ts - webpackHotmoduleReplacementPlugin and BundleAnalyzerPlugin runs only dev build

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -5,27 +5,34 @@ import ReactRefreshPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import { BuildOptions } from './types/config';
 
-export const buildPlugins = ({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] => [
-  new HtmlWebpackPlugin({
-    template: paths.html,
-  }),
+export const buildPlugins = ({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] => {
+  const plugins = [
+    new HtmlWebpackPlugin({
+      template: paths.html,
+    }),
 
-  new webpack.ProgressPlugin(),
-  new MiniCssExtractPlugin({ // ? для компиляции css свойств в отдельные css файлы
-    filename: 'css/[name].[contenthash:8].css',
-    chunkFilename: 'css/[name].[contenthash:8].css',
-  }),
+    new webpack.ProgressPlugin(),
+    new MiniCssExtractPlugin({ // ? для компиляции css свойств в отдельные css файлы
+      filename: 'css/[name].[contenthash:8].css',
+      chunkFilename: 'css/[name].[contenthash:8].css',
+    }),
 
-  // ? Для прокидывания глобальных переменных по проекту
-  // (например, для i18n переключение debug режима по переменной isDev)
-  new webpack.DefinePlugin({
-    __IS_DEV__: JSON.stringify(isDev),
-    // ? также объявляем в декларации переменную src-app-types-global.d.ts
-  }),
+    // ? Для прокидывания глобальных переменных по проекту
+    // (например, для i18n переключение debug режима по переменной isDev)
+    new webpack.DefinePlugin({
+      __IS_DEV__: JSON.stringify(isDev),
+      // ? также объявляем в декларации переменную src-app-types-global.d.ts
+    }),
 
-  new ReactRefreshPlugin(), // ? для корректного обновления react при hot reload
-  new webpack.HotModuleReplacementPlugin(), // ? горячее обновление проекта без перезагрузки
-  new BundleAnalyzerPlugin({
-    openAnalyzer: false,
-  }), // ? плагин для анализа бандла
-];
+    new ReactRefreshPlugin(), // ? для корректного обновления react при hot reload
+  ];
+
+  if (isDev) {
+    plugins.push(new webpack.HotModuleReplacementPlugin()); // ? горячее обновление проекта без перезагрузки
+    plugins.push(new BundleAnalyzerPlugin({
+      openAnalyzer: false,
+    }));// ? плагин для анализа бандла
+  }
+
+  return plugins;
+};
